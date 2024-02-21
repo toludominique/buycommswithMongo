82e45bc942dd9ff5c411a3e28b8214b3b312bdf1
tolutope oguntola <tolutopeoguntola@yahoo.com>
2024-02-21 11:03:24 +0100

    edited cors backend

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.listen(8800, () => {
  app.use(express.json())
  app.use(cors({
 
-   origin: "http://localhost:3000",
+   origin: "https://buycommswith-mongo-frontend.vercel.app",
   
  }))
  app.use(bodyParser.json())
